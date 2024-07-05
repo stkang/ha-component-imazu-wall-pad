e87636ea69ee9557b43457c8ef4bed84a6a9fd3d
stkang90 <stkang90@neowiz.com>
2024-07-05 12:40:12 +0900

    feat: add dimmer

--- custom_components/imazu_wall_pad/light.py
+++ custom_components/imazu_wall_pad/light.py
@@ -2,24 +2,32 @@
 
 from typing import Any
 
-from wp_imazu.packet import LightPacket
-
-from homeassistant.components.light import LightEntity, ColorMode
+from homeassistant.components.light import ATTR_BRIGHTNESS, LightEntity, ColorMode
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from wp_imazu.packet import LightPacket, DimmingPacket
+
 from . import ImazuGateway, ImazuWallPadConfigEntry
 from .gateway import EntityData
 from .wall_pad import WallPadDevice
 
 SCAN_LIGHT_PACKETS = [
+    # Light
     "01190140100000",
     "01190140200000",
     "01190140300000",
     "01190140400000",
     "01190140500000",
     "01190140600000",
+    # Dimmer
+    "011a0142100000",
+    "011a0142200000",
+    "011a0142300000",
+    "011a0142400000",
+    "011a0142500000",
+    "011a0142600000",
 ]
 
 
@@ -35,6 +43,9 @@ async def async_setup_entry(
     def async_add_entity(entity_data: EntityData):
         if isinstance(entity_data.packet, LightPacket):
             entity_data.device = WPLight(gateway, Platform.LIGHT, entity_data.packet)
+            async_add_entities([entity_data.device])
+        elif isinstance(entity_data.packet, DimmingPacket):
+            entity_data.device = WPDimmer(gateway, Platform.LIGHT, entity_data.packet)
             async_add_entities([entity_data.device])
 
     entities = gateway.get_platform_entities(Platform.LIGHT)
@@ -55,7 +66,7 @@ async def async_setup_entry(
 class WPLight(WallPadDevice[LightPacket], LightEntity):
     """Representation of a Wall Pad light."""
 
-    _attr_supported_color_modes = ColorMode.ONOFF
+    _attr_supported_color_modes = {ColorMode.ONOFF}
     _attr_color_mode = ColorMode.ONOFF
 
     @property
@@ -71,4 +82,40 @@ class WPLight(WallPadDevice[LightPacket], LightEntity):
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn off light."""
         make_packet = self.packet.make_change_power(LightPacket.Power.OFF)
+        await super().async_send_packet(make_packet)
+
+
+class WPDimmer(WallPadDevice[DimmingPacket], LightEntity):
+    """Representation of a Wall Pad light."""
+
+    _attr_supported_color_modes = {ColorMode.BRIGHTNESS}
+    _attr_color_mode = ColorMode.BRIGHTNESS
+
+    @property
+    def is_on(self) -> bool:
+        """Return true if light is on."""
+        return (
+            "brightness" in self.packet.state and self.packet.state["brightness"] != 0
+        )
+
+    @property
+    def brightness(self) -> int:
+        """Return the brightness of this light between 0..255."""
+        if "brightness" not in self.packet.state:
+            return 0
+        return round(self.packet.state["brightness"] * 255 / 3)
+
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Turn on light."""
+        if ATTR_BRIGHTNESS in kwargs:
+            brightness = round(kwargs[ATTR_BRIGHTNESS] * 3 / 255)
+            make_packet = self.packet.make_change_brightness(brightness)
+        else:
+            make_packet = self.packet.make_change_brightness(1)
+
+        await super().async_send_packet(make_packet)
+
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        """Turn off light."""
+        make_packet = self.packet.make_change_brightness(0)
         await super().async_send_packet(make_packet)
